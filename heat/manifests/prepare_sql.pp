a9e15450bee05ef290a5573c51f1d91f7d3b6170
sl4shme <slashme@hp.com>
2015-12-16 03:23:29 +0000

    mysql install working

--- heat/manifests/prepare_sql.pp
+++ heat/manifests/prepare_sql.pp
@@ -1,18 +1,45 @@
 class heat::prepare_sql (
-#    $mysqlPass,
-#    $mysqlUser,
-#    $mysqlDb,
+    $mysqlPass,
+    $mysqlUser,
+    $mysqlDb,
     $mysqlRootPwd
 ) {
 
-  class { '::mysql::server':
-    root_password => $mysqlRootPwd
+  $realUser = "$mysqlUser@%"
+
+  class { 'mysql::server':
+    root_password => $mysqlRootPwd,
+    override_options => {
+      mysqld => { bind-address => '0.0.0.0'}
+    }
   }
 
-  class { '::mysql::client': }
+  class { 'mysql::client': }
 
+  mysql_user { $realUser:
+    ensure        => present,
+    password_hash => mysql_password($mysqlPass),
+    require => Class['mysql::server']
+  }
+
+  mysql_database { $mysqlDb:
+    ensure  => present,
+    require => Mysql_user[$realUser]
+  }
+
+  mysql_grant { "$realUser/$mysqlDb.*":
+    ensure     => present,
+    privileges => ["ALL"],
+    table      => "$mysqlDb.*",
+    user       => "$realUser",
+    require => Mysql_database[$mysqlDb]
+  }
 }
 
 class {'heat::prepare_sql':
-  mysqlRootPwd => "plop"
+  mysqlRootPwd => "plop",
+  mysqlUser => "plop@%",
+  mysqlDb => "plop",
+  mysqlPass => "plop"
 }
+
